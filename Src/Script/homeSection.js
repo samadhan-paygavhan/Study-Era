ea67e2040ec9226f65865174e752e0f82f4e5249
Samadhan Paygavhan <samapaygwhan@gmail.com>
2026-01-14 00:51:26 +0530

    Add Explore button functionality like when we hover explore button the pop up block when we particular course hover their topics show us like that

--- Src/Script/homeSection.js
+++ Src/Script/homeSection.js
@@ -1,23 +1,65 @@
 // Explore button
+// When hover explore button the block show otherwise hide
 
-let explore = document.querySelector(".explore");
+const exploreBtn = document.querySelector(".explore");
+const container = document.querySelector(".exploreContainer");
 
-explore.addEventListener("click", ()=> {
-    const section = document.getElementById("footer-section");
-    goToSection(section);
+// Show
+exploreBtn.addEventListener("mouseenter", () => {
+    container.style.display = "block";
 });
 
-function goToSection(targetElement) {
+container.addEventListener("mouseenter", () => {
+    container.style.display = "block";
+});
 
-    if(targetElement) {
-        targetElement.scrollIntoView ({
-            behaviar: 'smooth',
-            block: 'start'
-        });
-    } else {
-        console.log("Element with footer-section this Id not found!");
+// Hide
+container.addEventListener("mouseleave", () => {
+    container.style.display = "none";
+});
+
+
+// When we hover particular course their subtopics show inside the course block
+
+const courseExplore = document.querySelector('.courseExplore');
+
+const topicMap = {
+    'fullStackCourse': '.fullStackTopics',
+    'machineLearning': '.machineLearningTopics',
+    'dataScience': '.dataScienceTopics',
+    'cyberSecurity': '.cyberSecurityTopics',
+    'cloudComputing': '.cloudComputingTopics',
+    'UI/UX': '.UIUxTopics',
+    'communicationSkill': '.communicationSkillTopics'
+};
+
+courseExplore.addEventListener('mouseover', (e) => {
+    const courseDiv = e.target.closest('.courses');
+    if (!courseDiv) return;
+
+    const topicClass = courseDiv.classList[0]; 
+    const targetSelector = topicMap[topicClass];
+
+    if (targetSelector) {
+
+        document.querySelectorAll('.topics').forEach(div => div.style.display = 'none');
+
+        const targetElement = document.querySelector(targetSelector);
+        if (targetElement) {
+            targetElement.style.display = 'block';
+        }
     }
-}
+});
+
+
+courseExplore.addEventListener('mouseleave', () => {
+    document.querySelectorAll('.topics').forEach(div => {
+        div.style.display = "none";
+    });
+
+    document.querySelector(".fullStackTopics").style.display = "block";
+
+});
 
 
 // Image Slider Automatic after 15 seconds
